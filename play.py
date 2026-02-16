00bcbb226ff0ab590fe6b5de738bed16e58f59fd
Wibukaratans <wibukaratans@gmail.com>
2026-02-16 16:31:59 +0700

    Version 1.0

---
+++ play.py
@@ -0,0 +1,48 @@
+import random
+import os
+import time
+os.system('clear')
+#Tittle
+print("==================")
+print("==Guess A Number==")
+print("==================")
+#Maximum,minimum number and max round
+x = int(input("Silahkan input Angka Minimum: "))
+y = int(input("Silahkan input Angka Maksimum: "))
+round = int(input("Silahkan input maksimal Tebakan: "))
+
+number = random.randint(x , y)#System pick  number
+ronde = 1
+os.system("clear")
+while ronde <= round :
+ try : 
+   print("===================")
+   print(f'======Ronde {ronde}======')
+   print("===================")
+   print(f'Angka Minimum: {x}')
+   guess = int(input("Tebak Angka: "))
+   if guess == number :
+    print("===============")
+    print("==Kamu Menang==")
+    print("===============")
+    break
+   else :
+     if guess > number :
+      print("Terlalu Besar")
+      ronde += 1
+      time.sleep(2)
+      os.system("clear")
+     else:
+      print("Terlalu Kecil")
+      ronde += 1
+      time.sleep(2)
+      os.system("clear")
+   
+ except :
+  print("Input Number!")
+  if True: 
+   time.sleep(2)
+   os.system("clear")
+
+if ronde == round:
+   print(f'Kamu Kalah angkanya adalah {number}')
