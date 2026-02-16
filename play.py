eac2c07bcd12441cd42cdd78ad9e0a8fd8e3e66e
Wibukaratans <wibukaratans@gmail.com>
2026-02-16 20:47:16 +0700

    version 2.0

--- play.py
+++ play.py
@@ -3,46 +3,55 @@ import os
 import time
 os.system('clear')
 #Tittle
-print("==================")
-print("==Guess A Number==")
-print("==================")
+z=1
+while z==1:
+ z-=1
+ try:
+  print("==================")
+  print("==Guess A Number==")
+  print("==================")
 #Maximum,minimum number and max round
-x = int(input("Silahkan input Angka Minimum: "))
-y = int(input("Silahkan input Angka Maksimum: "))
-round = int(input("Silahkan input maksimal Tebakan: "))
+  x = int(input("Silahkan input Angka Minimum: "))
+  y = int(input("Silahkan input Angka Maksimum: "))
+  round = int(input("Silahkan input maksimal Tebakan: "))
 
-number = random.randint(x , y)#System pick  number
-ronde = 1
-os.system("clear")
-while ronde <= round :
- try : 
-   print("===================")
-   print(f'======Ronde {ronde}======')
-   print("===================")
-   print(f'Angka Minimum: {x}')
-   guess = int(input("Tebak Angka: "))
-   if guess == number :
-    print("===============")
-    print("==Kamu Menang==")
-    print("===============")
-    break
-   else :
-     if guess > number :
-      print("Terlalu Besar")
-      ronde += 1
-      time.sleep(2)
-      os.system("clear")
-     else:
-      print("Terlalu Kecil")
-      ronde += 1
-      time.sleep(2)
-      os.system("clear")
+  number = random.randint(x , y)#System pick  number
+  ronde = 1
+  os.system("clear")
+  while ronde <= round :
+   try : 
+    print("===================")
+    print(f'======Ronde {ronde}======')
+    print("===================")
+    print(f'Angka Minimum: {x}')
+    guess = int(input("Tebak Angka: "))
+    if guess == number :
+     print("===============")
+     print("==Kamu Menang==")
+     print("===============")
+     break
+    else :
+      if guess > number :
+       print("Terlalu Besar")
+       ronde += 1
+       time.sleep(1)
+       os.system("clear")
+      else:
+       print("Terlalu Kecil")
+       ronde += 1
+       time.sleep(1)
+       os.system("clear")
    
- except :
-  print("Input Number!")
-  if True: 
-   time.sleep(2)
+   except :
+    print("Input Number!")
+    if True: 
+     time.sleep(1)
+     os.system("clear")
+     
+   if ronde > round:
+    print(f'Kamu Kalah angkanya adalah {number}')
+ except:
+   print("Input Number!")
+   time.sleep(1)
    os.system("clear")
-
-if ronde == round:
-   print(f'Kamu Kalah angkanya adalah {number}')
+   z+=1
